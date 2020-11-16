60653d17215a55e0a79079309fc51fecc56a44a1
Bipeen Acharya <acharyab@seas.upenn.edu>
2020-11-16 15:53:13 -0800

    Section 2: Tests are just Go code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/acharyab15/test-with-go
+
+go 1.15
---
+++ sleep/what_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTmpExecutable(t *testing.T) {
+	// Causes the test to sleep
+	// Uncomment this to see it sleep
+	// time.Sleep(time.Minute)
+}
