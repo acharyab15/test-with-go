bfb8b2a340fa556de1eab2156c5a98973a62e24d
Bipeen Acharya <acharyab@seas.upenn.edu>
2021-01-28 15:14:18 -0800

    stripe: the charge endpoint

--- stripe/client.go
+++ stripe/client.go
@@ -6,25 +6,70 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
 const (
-	Version = "2018-09-24"
+	Version         = "2018-09-24"
+	DefaultCurrency = "usd"
 )
 
 type Customer struct {
-	ID string `json:"id"`
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	DefaultSource string `json:"default_source"`
+}
+
+type Charge struct {
+	ID             string `json:"id"`
+	Amount         int    `json:"amount"`
+	FailureCode    string `json:"failure_code"`
+	FailureMessage string `json:"failure_message"`
+	Paid           bool   `json:"paid"`
+	Status         string `json:"status"`
 }
 
 type Client struct {
 	Key string
 }
 
-func (c *Client) Customer(tok string) (*Customer, error) {
+func (c *Client) Customer(tok, email string) (*Customer, error) {
 	endpoint := "https://api.stripe.com/v1/customers"
 	v := url.Values{}
 	v.Set("source", tok)
+	v.Set("email", email)
+	req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(v.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Stripe-Version", Version)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(c.Key, "")
+	httpClient := http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var cus Customer
+	err = json.Unmarshal(body, &cus)
+	if err != nil {
+		return nil, err
+	}
+	return &cus, nil
+}
+
+func (c *Client) Charge(customerID string, amount int) (*Charge, error) {
+	endpoint := "https://api.stripe.com/v1/charges"
+	v := url.Values{}
+	v.Set("customer", customerID)
+	v.Set("amount", strconv.Itoa(amount))
+	v.Set("currency", DefaultCurrency)
 	req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(v.Encode()))
 	if err != nil {
 		return nil, err
@@ -43,10 +88,11 @@ func (c *Client) Customer(tok string) (*Customer, error) {
 		return nil, err
 	}
 	fmt.Println(string(body))
-	var cus Customer
-	err = json.Unmarshal(body, &cus)
+	var chg Charge
+	err = json.Unmarshal(body, &chg)
 	if err != nil {
 		return nil, err
 	}
-	return &cus, nil
+	return &chg, nil
+
 }
--- stripe/client_test.go
+++ stripe/client_test.go
@@ -1,18 +1,31 @@
 package stripe_test
 
 import (
+	"flag"
 	"strings"
 	"testing"
 
 	"github.com/acharyab15/test-with-go/stripe"
 )
 
+var (
+	apiKey string
+)
+
+func init() {
+	flag.StringVar(&apiKey, "key", "", "Your TEST secret key for the Stripe API. If present, integration tests will be run using this key.")
+}
+
 func TestClient_Customer(t *testing.T) {
+	if apiKey == "" {
+		t.Skip("No API key provided")
+	}
 	c := stripe.Client{
-		Key: "sk_test_4eC39HqLyjWDarjtT1zdp7dc",
+		Key: apiKey,
 	}
 	tok := "tok_amex"
-	cus, err := c.Customer(tok)
+	email := "test@bipeen.com"
+	cus, err := c.Customer(tok, email)
 	if err != nil {
 		t.Fatalf("Customer() err = %s; want i%v", err, nil)
 	}
@@ -21,5 +34,39 @@ func TestClient_Customer(t *testing.T) {
 	}
 	if !strings.HasPrefix(cus.ID, "cus_") {
 		t.Errorf("Customer() ID = %s; want prefix %q", cus.ID, "cus_")
+	}
+	if !strings.HasPrefix(cus.DefaultSource, "card_") {
+		t.Errorf("Customer() DefaultSource = %s; want prefix %q", cus.DefaultSource, "card_")
+	}
+	if cus.Email != email {
+		t.Errorf("Customer() Email = %s; want %s", cus.Email, email)
+	}
+}
+
+func TestClient_Charge(t *testing.T) {
+	if apiKey == "" {
+		t.Skip("No API key provided")
+	}
+	c := stripe.Client{
+		Key: apiKey,
+	}
+	// Create a customer for the test
+	tok := "tok_amex"
+	email := "test@test.com"
+	cus, err := c.Customer(tok, email)
+	if err != nil {
+		t.Fatalf("Customer() err = %v' want nil", err)
+	}
+	_ = cus
+	amount := 1234
+	charge, err := c.Charge("cus_", amount)
+	if err != nil {
+		t.Fatalf("Charge() err = %s; want i%v", err, nil)
+	}
+	if charge == nil {
+		t.Fatalf("Charge() = nil; want non-nil value")
+	}
+	if charge.Amount != amount {
+		t.Errorf("Charge() Amount = %d; want %d", charge.Amount, amount)
 	}
 }
